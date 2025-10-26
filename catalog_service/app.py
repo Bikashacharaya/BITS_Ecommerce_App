850f652755c875d7f7e2d65209ee12ec73b32dee
Shivani99-max <shivaniraja19@gmail.com>
2025-10-26 17:01:13 +0530

    committing inventory service

--- catalog_service/app.py
+++ catalog_service/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 # Register Blueprint
 app.register_blueprint(product_bp, url_prefix='/v1')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=18082, debug=True)
